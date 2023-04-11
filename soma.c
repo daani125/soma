ca8b5619138b13057feef09372746550b2007897
daani125 <128492691+daani125@users.noreply.github.com>
2023-04-11 12:13:06 -0300

    Create soma.c

---
+++ soma.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+	int num1,num2,total;
+	
+
+	printf("Digite o primeiro numero: ");
+	scanf("%d",&num1);
+	
+
+	printf("Digite o primeiro numero: ");
+	scanf("%d",&num2);
+	
+	total = num1+num2;
+	
+	printf("O resultado e = %d", total);
+	
+	return (0);
+}
